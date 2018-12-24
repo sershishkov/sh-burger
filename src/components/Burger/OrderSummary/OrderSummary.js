d66cd3eea0ba27e88e447ab889fc761cc29441f6
sershishkov <ser.shishkov@gmail.com>
2018-12-24 14:11:18 +0200

    8-142

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary =Object.keys(props.ingredients)
+class OrderSummary extends Component {
+//это должен быть функциональный компонент, 
+//сделали классом для тестирования жизненных циклов
+  componentWillUpdate(){
+    console.log('[OrederSummary] WillUpdate()');
+  }
+
+  render(){
+    const ingredientSummary =Object.keys(this.props.ingredients)
       .map(ingrdKey => {
         return (<li key={ingrdKey}>
-          <span style={{textTransforn:'capitalize'}}>{ingrdKey}</span> : {props.ingredients[ingrdKey]}
+          <span style={{textTransforn:'capitalize'}}>{ingrdKey}</span> : {this.props.ingredients[ingrdKey]}
           </li>
           )
       });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingradients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
-      <p>Continue to Checkout?</p>
-       <Button btnType="Danger" clicked={props.purchaseCanceled}>CANCEL</Button>
-       <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-    </Aux>
-  )
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingradients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong> </p>
+        <p>Continue to Checkout?</p>
+         <Button btnType="Danger" clicked={this.props.purchaseCanceled}>CANCEL</Button>
+         <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+      </Aux>
+    )
+  }
 }
 
-export default orderSummary;
+
+
+export default OrderSummary;
