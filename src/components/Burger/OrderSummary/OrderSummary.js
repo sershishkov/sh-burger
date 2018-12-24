21166df65d126cb4b7ce23dd13c33b9e8c1c65a9
sershishkov <ser.shishkov@gmail.com>
2018-12-24 14:27:55 +0200

    8-145 finish 8

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
+
 class OrderSummary extends Component {
 //это должен быть функциональный компонент, 
 //сделали классом для тестирования жизненных циклов
