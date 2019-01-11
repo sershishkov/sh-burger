7ae6a05390d03cbff2a62904fc0bcb2c716d8d90
sershishkov <ser.shishkov@gmail.com>
2019-01-11 10:26:26 +0200

    12-210

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -12,11 +12,11 @@ const CheckoutSummary = (props) => {
       </div>
       <Button 
       btnType="Danger"
-      clicked
+      clicked={props.checkoutCancelled}
       >CANCEL</Button>
       <Button 
       btnType="Success"
-      clicked
+      clicked={props.checkoutContinued}
       >CONTINUE</Button>
     </div>
   );
