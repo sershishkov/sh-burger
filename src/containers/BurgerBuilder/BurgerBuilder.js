d676d970feab9c564b88919410ac8ae5eeff717a
sershishkov <ser.shishkov@gmail.com>
2019-01-11 12:10:52 +0200

    12-213

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -40,32 +40,12 @@ const INGREDIENT_PRICES = {
 
   purchaseContinueHandler = ()=>{
     // alert('You continue');
-    // this.setState({loading:true});
-    // const order = {
-    //   ingredients:this.state.ingredients,
-    //   price:this.state.totalPrice,
-    //   customer:{
-    //     name: "Ser Shishkov",
-    //     address:{
-    //       street:"Main street 1",
-    //       zipCode:'69005',
-    //       country:"Ukraine"
-    //     },
-    //     email:"test@test.com"
-    //   },
-    //   deliveryMethod:"fastest"
-    // }
-    // axios.post('/orders.json', order)
-    // .then(response => {
-    //   this.setState({loading:false, purchasing:false });
-    // })
-    // .catch(error => {
-    //   this.setState({loading:false, purchasing:false });
-    // });
+    
     const queryParams = [];
     for (let i in this.state.ingredients){
       queryParams.push(encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i]));
     }
+    queryParams.push('price='+ this.state.totalPrice);
     const queryString = queryParams.join("&");
 
     this.props.history.push({
