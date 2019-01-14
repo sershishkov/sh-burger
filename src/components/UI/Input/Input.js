a2f85cb22f8237e94f9c8ef60de021e69659c2db
sershishkov <ser.shishkov@gmail.com>
2019-01-14 12:35:54 +0200

    13-231

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,12 +4,13 @@ import classes from './Input.css';
 const Input = (props) => {
   let inputElement = null;
   const inputClasses = [classes.InputElement];
-  if(props.invalid && props.shouldValidate) {
+  if(props.invalid && props.shouldValidate && props.touched) {
     inputClasses.push(classes.Invalid);
   }
 
   switch(props.elementType){
-    case ('input'):inputElement=<input 
+    case ('input'):inputElement=<input
+                                  autoComplete="new-password" 
                                   className={inputClasses.join(' ')} 
                                   {...props.elementConfig} 
                                   value={props.value}
